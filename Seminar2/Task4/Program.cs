e179dee8c3655421597b6f057e13bea3c06623b0
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-05 16:43:05 +0300

    Task4 in Seminar 2 updated

--- Seminar2/Task4/Program.cs
+++ Seminar2/Task4/Program.cs
@@ -5,8 +5,26 @@
 // 46 -> нет 
 // 161 -> да
 
-System.Console.WriteLine("Введите число:");
-int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 7 == 0 && number % 23 == 0)
+// {
+//     System.Console.WriteLine($"{number} кратно 7 и 23");
+// }
+// else
+// {
+//     System.Console.WriteLine($"{number} не кратно 7 и 23");
+// }
+
+
+int Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt("Введите число:");
 if (number % 7 == 0 && number % 23 == 0)
 {
     System.Console.WriteLine($"{number} кратно 7 и 23");
