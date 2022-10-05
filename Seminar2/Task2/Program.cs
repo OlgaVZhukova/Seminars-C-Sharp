f7a54b3021df91e05c39ac475b6acc49f2bd6d26
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-05 16:01:44 +0300

    Task2 in Seminar 2 updated

--- Seminar2/Task2/Program.cs
+++ Seminar2/Task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит 
+// случайное число из отрезка [10, 99] и показывает 
+// наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+System.Console.WriteLine(number);
+int result1 = number / 10;
+int result2 = number % 10;
+if (result1 < result2)
+{
+    System.Console.WriteLine($"Вторая цифра {result2} больше первой {result1}");
+}
+else
+{
+    System.Console.WriteLine($"Первая цифра {result1} больше второй {result2}");
+}
+if (result1 == result2) { System.Console.WriteLine("Цифры равны между собой"); }
+
+// непонятно, если цифры равны, как лучше сделать
