d6870197bae2486c6c5a06bce9b2a72ff4115e2a
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-05 15:17:56 +0300

    Task 1 in Seminar 2 updated

--- Seminar2/Task1/Program.cs
+++ Seminar2/Task1/Program.cs
@@ -5,4 +5,15 @@
 // 782 -> 2
 // 918 -> 8
 
+System.Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 > 99 & num1 < 1000)
+{
+    int result = num1 % 10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+}
 
