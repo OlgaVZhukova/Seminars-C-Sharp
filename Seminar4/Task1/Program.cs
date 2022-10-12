6553153ae4566a375311b144894a8d9cfebadadf
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-12 19:12:40 +0300

    Seminar 4 Task 1 added

---
+++ Seminar4/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает 
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int InputNumber(string message)
+{
+    Console.Write(message);
+    string str = Console.ReadLine();
+    return int.Parse(str); // return Convert.ToInt32(str);
+}
+
+int SumNumbers(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 1)
+    {
+        System.Console.WriteLine("Число меньше единицы, сумма не считается");
+        return false;
+    }
+    return true;
+}
+
+int n = InputNumber("Введите число > ");
+if (ValidateNumber(n))
+{
+    int result = SumNumbers(n);
+    System.Console.WriteLine($"Сумма чисел от 1 до {n} равна {result}");
+}
