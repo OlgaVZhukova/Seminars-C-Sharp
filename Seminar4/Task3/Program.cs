3e70d366e0fa5329bf2ce05c34372bab8edae64b
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-12 19:51:10 +0300

    Seminar 4 Task 3 added

---
+++ Seminar4/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая 
+// принимает на вход число N и выдаёт 
+// произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int InputNumber(string msg)
+{
+    System.Console.WriteLine(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = InputNumber("Введите число: ");
+
+int multiply = 1;
+for (int i = 1; i <= number; i++)
+{
+    multiply = multiply * i;
+}
+
+
+System.Console.WriteLine($"Выводим факториал: {multiply}");
+
+
+
+
