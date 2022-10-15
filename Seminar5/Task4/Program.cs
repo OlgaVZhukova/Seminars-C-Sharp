3b55a78c8d05daeb6d46a73f6a4eda8a0213ee94
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-15 16:12:53 +0300

    Seminar 5 Task 4 added

---
+++ Seminar5/Task4/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в 
+// отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray(int length, int minLimit, int maxLimit)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int[] myArray = CreateArray(10, 0, 200);
+PrintArray(myArray);
+
+// int count = 0;
+// for (int i = 0; i < myArray.Length; i++)
+// {
+//     if (myArray[i] >= 10 && myArray[i] <= 99)
+//     {
+//         count++;
+//     }
+// }
+// Вместо этого оберенем все это в функцию:
+
+int CountNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+System.Console.WriteLine($"Количество двузначных элементов: {CountNumbers(myArray)}");
+
