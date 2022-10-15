f67381aae98caa1be91007ce4c30f0756f733415
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-15 13:02:31 +0300

    Seminar 5 Task 1 updated

--- Seminar5/Task1/Program.cs
+++ Seminar5/Task1/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив из 12 элементов, заполненный 
+// случайными числами из промежутка [-9, 9]. Найдите сумму 
+// отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма 
+// положительных чисел равна 29, сумма отрицательных равна 
+// -20.
+
+int[] CreateArray(int length, int minLimit, int maxLimit)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int SumPositive(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int SumNegative(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int[] arr = CreateArray(8, -9, 9);
+PrintArray(arr);
+int sumPlus = SumPositive(arr);
+int sumMinus = SumNegative(arr);
+System.Console.WriteLine($"Сумма положительных элементов = {sumPlus}, сумма отрицательных элементов = {sumMinus}");
+
