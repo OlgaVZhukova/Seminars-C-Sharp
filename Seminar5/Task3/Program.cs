243efde7b596bb35aa1d487e81539c176a5ab851
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-15 15:35:57 +0300

    Seminar 5 Task 3 added

---
+++ Seminar5/Task3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте массив. Напишите программу, которая 
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArray(int length, int minLimit, int maxLimit)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool SpecifyNumber(int[] array, int value)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == value)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+int[] array = CreateArray(8, -9, 9);
+PrintArray(array);
+int value = Prompt("Введите число:");
+System.Console.WriteLine(SpecifyNumber(array, value));
+
