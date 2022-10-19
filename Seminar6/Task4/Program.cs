cf227616f2085c290e213fc3a38229309e707c92
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-19 20:08:17 +0300

    Seminar 6 Task 4 added

---
+++ Seminar6/Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿// : Не используя рекурсию, выведите первые N чисел 
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] RandomArrGen()
+{
+    int length = new Random().Next(1, 15);
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+    }
+}
+
+int Prompt()
+{
+    System.Console.WriteLine("Введите число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+int count = Prompt();
+
+int tmp1 = 0;
+int tmp2 = 1;
+while (count > 0)
+{
+    int tmp = tmp1 + tmp2;
+    System.Console.WriteLine($"{tmp1}");
+    tmp1 = tmp2;
+    tmp2 = tmp;
+    count--;
+}
+
