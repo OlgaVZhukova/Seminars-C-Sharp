f29843cf7260585ab76f12b48fbd3e10e68105da
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-21 20:25:44 +0300

    Seminar 7 Task 1 added

---
+++ Seminar7/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] CreateArray(int lenRows, int lenColumns)
+{
+    int[,] array = new int[lenRows, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+    }
+}
+
+
