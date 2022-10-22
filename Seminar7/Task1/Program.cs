de36dcd756fc2261d7f2c0f3b4c5b9cf200e0630
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-22 13:05:22 +0300

    Seminar 7 Task 1 updated

--- Seminar7/Task1/Program.cs
+++ Seminar7/Task1/Program.cs
@@ -9,23 +9,30 @@ int[,] CreateArray(int lenRows, int lenColumns)
 {
     int[,] array = new int[lenRows, lenColumns];
     Random random = new Random();
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-5, 6);
+        }
     }
+    return array;
 }
 
-
+//выведем теперь это на экран
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            l
+            System.Console.Write($"{array[i, j]}\t"); //благодаря \t (табуляции) будут более-менее ровные колонки, даже с 2-х-значными числами
         }
+        System.Console.WriteLine(); // отображение простой строчки после каждой строчки
     }
 }
 
-
+//теперь нужно массив сгенерировать
+int[,] array = CreateArray(3, 4);
+PrintArray(array);
 
