fcde76fe01437e61dd6bb4a3d678e84ef5b526af
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-23 10:21:00 +0300

    Seminar 7 Task 8 added

---
+++ Seminar7/Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывести последовательность чисел: одна 1, две 2, три 3, и т.д. до 10 - десять десяток.
+
+int Prompt()
+{
+    System.Console.WriteLine("Введите число:");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+int number = Prompt();
+
+for (int i = 1; i <= number; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        System.Console.Write($"{i}, ");
+    }
+}
