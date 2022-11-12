085dc57dd19a4b7aa3b667fcd36b2cb02ae23698
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-11-12 19:19:42 +0300

    Seminar 9 Task 4 updated

--- Seminar9/Task4/Program.cs
+++ Seminar9/Task4/Program.cs
@@ -2,4 +2,25 @@
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
 
+int Prompt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int Power(int num, int exp)
+{
+    if (exp <= 0)
+    {
+        return 1;
+    }
+
+    return num * Power(num, exp - 1);
+}
+
+int num = Prompt("Введите число A");
+int exp = Prompt("Введите число B");  // exponentiation
+
+int result = Power(num, exp);
+System.Console.WriteLine(result);
 
