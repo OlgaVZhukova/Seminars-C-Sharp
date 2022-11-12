53b907efb8fc82f47a4fc0b0800234c37631134c
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-11-12 19:42:24 +0300

    Seminar 9 Task 5 added

---
+++ Seminar9/Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 5: Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+bool Power(int num)
+{
+    if (num <= 2)
+    {
+        return true;
+    }
+    return (num % 2 == 0) && (Power(num / 2));
+}
+
+int num = Prompt("Введите число");
+bool result = Power(num);
+System.Console.WriteLine(result);
