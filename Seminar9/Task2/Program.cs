adcb595d0c37f78e35c147e4be0386ad089688fe
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-11-12 13:34:06 +0300

    Seminar 9 Task 2 updated

--- Seminar9/Task2/Program.cs
+++ Seminar9/Task2/Program.cs
@@ -2,4 +2,23 @@
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
 
+int Prompt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
 
+void PrintNumbers(int m, int n)
+{
+    if (m > n)
+    {
+        return;
+    }
+
+    System.Console.Write(m + " ");
+    PrintNumbers(m + 1, n);
+}
+
+int m = Prompt("Введите число M");
+int n = Prompt("Введите число N");
+PrintNumbers(m, n);
