72c3706d885c16942a35d264766eea67cf0c6cfa
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-11-12 13:25:49 +0300

    Seminar 9 Task 1 updated

--- Seminar9/Task1/Program.cs
+++ Seminar9/Task1/Program.cs
@@ -2,4 +2,24 @@
 // N = 5 -> "1, 2, 3, 4, 5"
 // N = 6 -> "1, 2, 3, 4, 5, 6"
 
+int Prompt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
 
+// пишем рекурсивную функцию, которая будет считать чила в промежутке от 1 до N
+
+void PrintNumbers(int number)
+{
+    if (number <= 0)
+    {
+        return;
+    }
+
+    PrintNumbers(number - 1);
+    System.Console.Write(number + " ");
+}
+
+int num = Prompt("Введите число");
+PrintNumbers(num);
