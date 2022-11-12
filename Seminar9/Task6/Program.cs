f128fd6c3d056a6335534baf9aba7bf152029073
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-11-12 19:52:53 +0300

    Seminar 9 Task 6 added

---
+++ Seminar9/Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 6: Проверка на простое число: 
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+bool Power(int num, int num1)
+{
+    if (num1 <= 1)
+    {
+        return true;
+    }
+    return (num % (num1) != 0) && Power(num, num1 - 1);
+}
+
+int num = Prompt("Введите число");
+bool result = Power(num, num - 1);
+System.Console.WriteLine(result);
+
