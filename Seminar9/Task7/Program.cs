86509bce2cd73c76511688ee2602058c5b08f6ea
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-11-12 20:08:09 +0300

    Seminar 9 Task 7 added

---
+++ Seminar9/Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Степень двойки (не рекурсией)
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+bool IsBinary(int num)
+{
+    return ((num - 1) & num) == 0;
+}
+
+int num = Prompt("Введите число");
+System.Console.WriteLine(IsBinary(num));
+
